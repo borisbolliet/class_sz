13f23edc1522c4a2a63517eacb1ee00e02f16c13
borisbolliet <boris.bolliet@gmail.com>
2023-03-25 15:18:19 +0000

    removing everything goodbye

--- python/classy_szfast/classy_szfast/__init__.py
+++
@@ -1,6 +0,0 @@
-from classy_szfast.classy_szfast import classy_szfast
-# from classy_szfast.classy_sz import classy_sz
-from .config import *
-from .utils import *
-from .cosmopower import *
-from .pks_and_sigmas import *
